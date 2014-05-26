128ca46ca800f1cca383f8d29ee2e7ab473348ff
Ash-girl Chapfuwa <ashgirlchapfuwa@gmail.com>
2014-05-26 12:03:25 -0300

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularjsAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
