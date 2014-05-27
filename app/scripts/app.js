8190eb24cf565aedc910686653f5a7008473664d
Ash-girl Chapfuwa <ashgirlchapfuwa@gmail.com>
2014-05-27 20:09:05 -0300

    fix js syntax

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,8 +5,9 @@ var app = angular.module('angularjsAppApp', [
     'ngCookies',
     'ngSanitize',
     'ngRoute'
-  ])
-  app.config(function ($routeProvider) {
+  ]);
+
+app.config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/posts.html',
--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -7,9 +7,10 @@ app.controller('PostsCtrl', function($scope){
   $scope.submitPost = function(){
     $scope.posts.push($scope.post);
     $scope.post = {url: 'http://', title: ''};
-  }
+  };
 
   $scope.deletePost = function(index){
     $scope.posts.splice(index, 1);
-  }
+  };
+
 });
