e319aae377218b3bfe7129184ac124b2660514ec
Ash-girl Chapfuwa <ashgirlchapfuwa@gmail.com>
2014-05-28 17:45:17 -0300

    handle errors

--- app/scripts/controllers/auth.js
+++ app/scripts/controllers/auth.js
@@ -13,13 +13,20 @@ app.controller('AuthCtrl', function($scope, $location, Auth){
   $scope.login = function(){
     Auth.login($scope.user).then(function(){
       $location.path('/');
+    }, function(error){
+      $scope.error = error.toString();
+      console.log($scope.error)
     });
   };
+  
 
   $scope.register = function() {
     Auth.register($scope.user).then(function(authUser){
-      console.log(authUser);
       $location.path('/');
+      console.log(authUser);
+    }, function(error){
+      $scope.error = error.toString();
+      console.log($scope.error);
     });
   };
 });
