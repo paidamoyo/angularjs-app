384008f701b7cbcb0de49dc84f2dcaf2615e66d3
Ash-girl Chapfuwa <ashgirlchapfuwa@gmail.com>
2014-05-28 18:54:26 -0300

    set username

--- app/scripts/services/users.js
+++ app/scripts/services/users.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.factory('User', function($firebase, FIREBASE_URL, Auth){
+app.factory('User', function($firebase, $rootScope, FIREBASE_URL, Auth){
   var ref = new Firebase(FIREBASE_URL + 'users');
 
   var users = $firebase(ref);
@@ -13,9 +13,40 @@ app.factory('User', function($firebase, FIREBASE_URL, Auth){
         $priority: authUser.uid
       };
 
-      users.$save(username);
+      users.$save(username).then(function(){
+        setCurrentUser(username);
+      });
+    },
+  
+    findByUsername: function(username) {
+      if(username) {
+        return users.$child(username);
+      } 
+    },
+
+    getCurrent: function(){
+      return $rootScope.currentUser;
+    },
+
+    signedIn: function(){
+      return $rootScope.currentUser !== undefined;
     }
   };
 
+  function setCurrentUser(username) {
+    $rootScope.currentUser = User.findByUsername(username);
+  }
+
+  $rootScope.$on('$firebaseSimpleLogin:login', function(e, authUser){
+    var query = $firebase(ref.startAt(authUser.uid).endAt(authUser.uid));
+
+    query.$on('loaded', function(){
+      setCurrentUser(query.$getIndex()[0]);
+    });
+  });
+  
+  $rootScope.$on('firebaseSimpleLogin:logout', function(){
+    delete $rootScope.currentUser;
+  });
   return User;
 });
